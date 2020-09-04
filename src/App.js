13da7b6e2f55a4d0e9842de28bdee8b9c069f73a
Kyuho Lee <kyuhochoilee@gmail.com>
2020-09-03 22:28:49 -0400

    working on amplitude

--- src/App.js
+++ src/App.js
@@ -4,12 +4,32 @@ import {
   Switch,
   Route,
   Link,
+  useRouteMatch,
   Outlet,
   useParams,
   withRouter
 } from "react-router-dom";
 import Bar from "./components/Bar"
 import Home from "./components/Home"
+import About from "./components/About"
+import Music from "./components/Music"
+import Design from "./components/Design"
+
+
+const App = withRouter(({ location }) => (
+  <div>
+    {location.pathname !== "/" && <Bar />}
+
+    <Switch>
+      <Route exact path="/"><Home /></Route>
+      <Route path="/about"><About /></Route>
+      <Route path="/music" ><Music /></Route>
+      <Route path="/design" ><Design /></Route>
+      <Route path="*" ></Route><NotFound />
+    </Switch>
+  </div>
+)
+)
 
 function NotFound() {
   return (
@@ -19,65 +39,6 @@ function NotFound() {
     </div>
   );
 }
-
-// function Home() {
-//   return (
-//     <div>
-//       <h1>Welcome Home!</h1>
-//     </div>
-//   );
-// }
-function About() {
-  return (
-    <div>
-      <h1>Welcome About!</h1>
-    </div>
-  );
-}
-function Music() {
-  return (
-    <div>
-      <h1>Welcome Music!</h1>
-    </div>
-  );
-}
-
-function MusicIndex() {
-  return (
-    <div>
-      <h1>Welcome Music!</h1>
-    </div>
-  );
-}
-
-function MusicInfo() {
-  return (
-    <div>
-      <h1>Welcome Music!</h1>
-    </div>
-  );
-}
-
-const App = withRouter(({location}) => (
-  <div>
-    {location.pathname !== "/" && <Bar/>}
-
-  <Switch>
-    <Route exact path="/"><Home /></Route>
-    <Route path="about"> <About /> </Route>
-    <Route path="music" ><Music />
-      <Route exact path="/" ><MusicIndex /></Route>
-      <Route path=":slug" ><MusicInfo /></Route>
-    </Route>
-    {/* <Route path="design" ><Design />
-      <Route exact path="/" ><DesignIndex /></Route>
-      <Route path=":slug" ><DesignInfo /></Route>
-    </Route> */}
-    <Route path="*" ></Route><NotFound />
-  </Switch>
-</div>
-)
-)
 
 export default App
 
